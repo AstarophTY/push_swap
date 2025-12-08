613d84d9bcebec4bdf2a55f542f358c9c2bbff50
AstarophTY <astarophty@gmail.com>
2025-12-08 15:13:56 +0100

    add compilation libft

--- libs/libft/includes/libft.h
+++ libs/libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: sgil--de <sgil--de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 12:32:59 by sgil--de          #+#    #+#             */
-/*   Updated: 2025/11/10 15:39:48 by sgil--de         ###   ########.fr       */
+/*   Updated: 2025/12/08 14:59:21 by sgil--de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strcmp(const char *s1, const char *s2);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *s, const char *f, size_t n);
